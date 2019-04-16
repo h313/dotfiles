b28c8a3bc0558be00b83e624829549a970379787
Haoda Wang <harry@h313.info>
2019-04-15 18:31:17 -0700

    yee

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 # Start X at login
 if status is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
-    end
+  if test -z "$DISPLAY" -a $XDG_VTNR = 1
+	set -gx SSH_AUTH_SOCK (gnome-keyring-daemon --start --components=pkcs11,secrets,ssh | grep "^SSH_AUTH_SOCK" | awk -F "=" '{print $2}')
+  	exec sway
+  end
 end
