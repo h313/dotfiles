808115bd1264d4007e3967d3b408b619619a1e11
Haoda Wang <harry@h313.info>
2019-10-08 12:20:11 -0700

    yea

--- .config/fish/functions/swaystart.fish
+++ .config/fish/functions/swaystart.fish
@@ -10,7 +10,6 @@ set -x SDL_VIDEODRIVER wayland
 set -x _JAVA_AWT_WM_NONREPARENTING 1
 set -x EDITOR nvim
 set -x QT_QPA_PLATFORMTHEME qt5ct
-set -x QT_AUTO_SCREEN_SCALE_FACTOR 1
 set -gx SSH_AUTH_SOCK (gnome-keyring-daemon --start --components=pkcs11,secrets,ssh | grep "^SSH_AUTH_SOCK" | awk -F "=" '{print $2}')
 set -x XCURSOR_SIZE 16
 set -x GTK_IM_MODULE ibus
