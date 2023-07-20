32b605dc22b7b4ba8410b2ec49b30fd93082d5cf
Haoda Wang <harry@h313.info>
2023-07-20 10:36:06 -0700

    Remove null-ls

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -44,14 +44,6 @@ return require('packer').startup(function(use)
   })
 
   use {
-    'jose-elias-alvarez/null-ls.nvim',
-    config = function()
-        require('null-ls').setup()
-    end,
-    requires = { 'nvim-lua/plenary.nvim' },
-  }
-
-  use {
     'nvim-treesitter/nvim-treesitter',
     run = function()
       local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
