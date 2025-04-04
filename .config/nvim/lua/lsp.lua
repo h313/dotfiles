51a557fe681ca45397d642e4c538d1e276028e6b
Haoda Wang <harry@h313.info>
2025-04-04 18:37:35 +0000

    Go back to cmp.nvim
    This reverts commit d6647911a7e4b019abb7169b72c0ad39f1682362.

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,52 +1,114 @@
 local lspconfig = require('lspconfig')
+local luasnip = require('luasnip')
+local cmp = require('cmp')
 
 require('mason').setup()
 require('mason-lspconfig').setup()
 
-lspconfig['asm_lsp'].setup({})
-lspconfig['bashls'].setup({})
-lspconfig['clangd'].setup({})
-lspconfig['cssls'].setup({})
-lspconfig['dockerls'].setup({})
-lspconfig['gopls'].setup({})
-lspconfig['html'].setup({})
-lspconfig['jsonls'].setup({})
-lspconfig['ltex'].setup({})
+local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+lspconfig['asm_lsp'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['bashls'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['clangd'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['cssls'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['dockerls'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['gopls'].setup({
+  capabilities = cmp_capabilities
+})
+
+lspconfig['html'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['jsonls'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['ltex'].setup({
+  capabilities = cmp_capabilities
+})
 lspconfig['lua_ls'].setup({
   settings = {
     telemetry = {
       enable = false,
     }
   },
-
+  capabilities = cmp_capabilities
 })
-lspconfig['marksman'].setup({})
-lspconfig['neocmake'].setup({})
-lspconfig['pyright'].setup({})
+lspconfig['marksman'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['neocmake'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['pyright'].setup({
+  capabilities = cmp_capabilities
+})
 lspconfig['ruff'].setup({
   on_attach = function (client, buffer)
     client.server_capabilities.hoverProvider = false
   end,
 })
-lspconfig['rust_analyzer'].setup({})
-lspconfig['texlab'].setup({})
-lspconfig['ts_ls'].setup({})
-lspconfig['verible'].setup({})
-lspconfig['yamlls'].setup({})
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('my.lsp', {}),
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    if client:supports_method('textDocument/implementation') then
-      -- Create a keymap for vim.lsp.buf.implementation ...
-    end
-    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-    if client:supports_method('textDocument/completion') then
-      -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-      local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-      client.server_capabilities.completionProvider.triggerCharacters = chars
-      vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
-    end
-  end,
+lspconfig['rust_analyzer'].setup({
+  capabilities = cmp_capabilities
 })
+lspconfig['texlab'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['ts_ls'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['verible'].setup({
+  capabilities = cmp_capabilities
+})
+lspconfig['yamlls'].setup({
+  capabilities = cmp_capabilities
+})
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
